7c3f4b75c95be8180340d98811d29d1848c63c49
Felipe F Quintella <felipe@quintella.com>
2023-09-14 17:52:44 -0300

    INC

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,21 +1,106 @@
-# @summary A short summary of the purpose of this class
+# @summary This is the main class wich will make netrisk run with all components
 #
-# A description of what this class does
+# Makes netrisk run
+#
+# @parameters
+#
+# [*service_name*]
+#   The name of the service, directories and base containers
+#
+# [*version*]
+#   The version to be installed
+#
+# [*api_server*]
+#   The server from wich the api should be called
+#
+# [*api_protocol*]
+#   The protocol api will use [http or https (default)]
+#
+# [*api_port*]
+#   The port to use with the api
+#
+# [*website_server*]
+#   The server from wich the website should be called
+#
+# [*website_protocol*]
+#   The protocol website will use [http or https (default)]
+#
+# [*website_port*]
+#   The port to use with the website
+#
+# [*db_server*]
+#   The server running the dbms (only mariadb is supported as right now)
+#
+# [*db_port*]
+#   The port dbms is running
+#
+# [*db_schema*]
+#   The schema used on the dbms
+#
+# [*db_user*]
+#   The user used on the dbms
+#
+# [*db_password*]
+#   The password used on the dbms
+#
+# [*ssl_cert_file*]
+#   The certificate file path
+#
+# [*db_password*]
+#   The certificate file password
+#
+# [*logging*]
+#   The log level to be used on the application
+#
+# [*email_from*]
+#   The email from to use on sent messages
+#
+# [*email_server*]
+#   The smtp email server (no authentication is supported right now)
+#
+# [*email_port*]
+#   The smtp email port (no ssl is supported right now)
+#
+# [*enable_api*]
+#   If disabled the api container will not be run
+#
+# [*enable_website*]
+#   If disabled the website container will not be run
+#
+# [*enable_console*]
+#   If disabled the console container will not be run
+#
+# [*enable_saml*]
+#   If we should use saml configs for login
 #
 # @example
 #   include dockerapp_netrisk
 class dockerapp_netrisk(
   $service_name = 'netrisk',
   $version = 'latest',
+  $api_server = $::fqdn,
+  $api_protocol = 'https',
   $api_port = '5443',
-  $site_port = '443',
+  $website_port = '443',
+  $website_protocol = 'https',
+  $website_server = $::fqdn,
   $db_server = '',
   $db_port = '3306',
   $db_schema = 'netrisk',
   $db_user = 'netrisk',
   $db_password = '',
-  $ssl_cert_file = '',
-  $ssl_cert_pwd = ''
+  $api_ssl_cert_file = '',
+  $api_ssl_cert_pwd = '',
+  $website_ssl_cert_file = '',
+  $website_ssl_cert_pwd = '',
+  $logging = 'Information',
+  $email_from = 'netrisk@mail.com',
+  $email_server = 'localhost',
+  $email_port = 25,
+  $enable_api = true,
+  $enable_website = true,
+  $enable_console = true,
+  $enable_saml = false
 ) {
 
 include dockerapp::basedirs
@@ -119,4 +204,90 @@ class {"dockerapp":
         require => File[$conf_configdir],
       }
     } 
+    if ! defined(File[$conf_scriptsdir]) {
+      file{ $conf_scriptsdir:
+        ensure  => directory,
+        require => File[$base_app_scripts],
+      }
+    }
+    if ! defined(File[$conf_logsdir]) {
+      file{ $conf_logsdir:
+        ensure  => directory,
+        require => File[$base_app_logs],
+      }
+    }
+    if ! defined(File[$conf_logsdir_website]) {
+      file{ $conf_logsdir_website:
+        ensure  => directory,
+        owner   => 33,
+        group   => 33,
+        require => File[$conf_logsdir],
+      }
+    } 
+    if ! defined(File[$conf_logsdir_api]) {
+      file{ $conf_logsdir_api:
+        ensure  => directory,
+        owner   => 33,
+        group   => 33,
+        require => File[$conf_logsdir],
+      }
+    } 
+
+  if $db_server == '' { fail('db_server cannot be empty') }
+  if $db_password == '' { fail('db_password is mandatory') }
+  if $api_ssl_cert_file == '' { fail('api_ssl_cert_file is mandatory') }
+  if $api_ssl_cert_pwd == '' { fail('api_ssl_cert_pwd is mandatory') }
+  if $website_ssl_cert_file == '' { fail('website_ssl_cert_file is mandatory') }
+  if $website_ssl_cert_pwd == '' { fail('website_ssl_cert_pwd is mandatory') }
+
+  #API CONFIGS
+  $envs = [
+    "FACTER_ENABLE_SAML=${enable_saml}",
+    "FACTER_DBSERVER=${db_server}",
+    "FACTER_DBUSER=${db_user}",
+    "FACTER_DBPORT=${db_port}",
+    "FACTER_DBPASSWORD=${db_password}",
+    "FACTER_DBSCHEMA=${db_schema}",
+    "FACTER_NETRISK_URL=${api_protocol}//${api_server}:${api_port}",
+    "FACTER_SERVER_LOGGING=${logging}",
+    "FACTER_EMAIL_FROM=${email_from}",
+    "FACTER_EMAIL_SERVER=${email_server}",
+    "FACTER_EMAIL_PORT=${email_port}",
+    "FACTER_WEBSITE_PROTOCOL=${website_protocol}",
+    "FACTER_WEBSITE_HOST=${website_server}",
+    "FACTER_WEBSITE_PORT=${website_port}",
+  ]
+
+  file{"${conf_configdir_api}/certs":
+    ensure  => directory,
+    require => File[$conf_configdir_api],
+  }
+  -> file{"${conf_configdir_api}/certs/api.pfx":
+    ensure => present,
+    source => $api_ssl_cert_file,
+  }
+
+
+  $volumes_api = [
+    "${conf_configdir}/certs/api.pfx:/netrisk/api.pfx",
+  ]
+
+
+  #WEBSITE CONFIGS
+
+  file{"${conf_configdir_website}/certs":
+    ensure  => directory,
+    require => File[$conf_configdir_website],
+  }
+  -> file{"${conf_configdir_website}/certs/website.pfx":
+    ensure => present,
+    source => $website_ssl_cert_file,
+  }
+
+
+  $volumes_website = [
+    "${conf_configdir}/certs/website.pfx:/netrisk/website.pfx",
+  ]
+  
+    
 }
--- spec/classes/dockerapp_netrisk_spec.rb
+++ spec/classes/dockerapp_netrisk_spec.rb
@@ -2,36 +2,143 @@
 
 require 'spec_helper'
 
+
 describe 'dockerapp_netrisk' do
 
   let(:node) { 'node1.test.com' }
-  let(:params) do
-    {
-      service_name: 'nettest',
-      version: '1.4.1',
 
-    }
+  context 'without db_server' do
+    
+    let(:params) do
+      {
+        service_name: 'nettest',
+        version: '1.4.1',
+      }
+    end
+
+    it { is_expected.to compile.and_raise_error(/db_server cannot be empty/)}
   end
 
-  on_supported_os.each do |os, os_facts|
-    context "on #{os}" do
-      let(:facts) { os_facts }
+  context 'without db_password' do
+    
+    let(:params) do
+      {
+        service_name: 'nettest',
+        version: '1.4.1',
+        db_server: 'testedb',
+      }
+    end
 
-      it { is_expected.to compile.with_all_deps }
-      it { is_expected.to contain_class('dockerapp').with(
-        manage_docker: false
-        ) }
+    it { is_expected.to compile.and_raise_error(/db_password is mandatory/)}
+  end
 
-      it { is_expected.to contain_file('/srv/application-data/nettest') }
-      it { is_expected.to contain_file('/srv/application-data/nettest/backups') }
-      it { is_expected.to contain_file('/srv/application-data/nettest/api') }
-      it { is_expected.to contain_file('/srv/application-data/nettest/website') }
-      it { is_expected.to contain_file('/srv/application-config/nettest') }
-      it { is_expected.to contain_file('/srv/application-config/nettest/api') }
-      it { is_expected.to contain_file('/srv/application-config/nettest/configurations') }
-      it { is_expected.to contain_file('/srv/application-config/nettest/ssl') }
-      it { is_expected.to contain_file('/srv/application-config/nettest/website') }
+  context 'without api_ssl_cert_file' do
+    
+    let(:params) do
+      {
+        service_name: 'nettest',
+        version: '1.4.1',
+        db_server: 'testedb',
+        db_password: 'testepwd',
+      }
+    end
 
+    it { is_expected.to compile.and_raise_error(/api_ssl_cert_file is mandatory/)}
+  end
+
+  context 'without api_ssl_cert_pwd' do
+    
+    let(:params) do
+      {
+        service_name: 'nettest',
+        version: '1.4.1',
+        db_server: 'testedb',
+        db_password: 'testepwd',
+        api_ssl_cert_file: '/sslfile.pfx',
+      }
+    end
+
+    it { is_expected.to compile.and_raise_error(/api_ssl_cert_pwd is mandatory/)}
+  end
+
+  context 'without website_ssl_cert_file' do
+    
+    let(:params) do
+      {
+        service_name: 'nettest',
+        version: '1.4.1',
+        db_server: 'testedb',
+        db_password: 'testepwd',
+        api_ssl_cert_file: '/sslfile.pfx',
+        api_ssl_cert_pwd: '123',
+      }
+    end
+
+    it { is_expected.to compile.and_raise_error(/website_ssl_cert_file is mandatory/)}
+  end
+
+    context 'without website_ssl_cert_pwd' do
+    
+    let(:params) do
+      {
+        service_name: 'nettest',
+        version: '1.4.1',
+        db_server: 'testedb',
+        db_password: 'testepwd',
+        api_ssl_cert_file: '/sslfile.pfx',
+        api_ssl_cert_pwd: '123',
+        website_ssl_cert_file: '/ws_sslfile.pfx',
+      }
+    end
+
+    it { is_expected.to compile.and_raise_error(/website_ssl_cert_pwd is mandatory/)}
+  end
+
+
+  context 'working example' do
+  
+    let(:params) do
+      {
+        service_name: 'nettest',
+        version: '1.4.1',
+        db_server: 'testedb',
+        db_password: 'testepwd',
+        api_ssl_cert_file: '/sslfile.pfx',
+        api_ssl_cert_pwd: '123',
+        website_ssl_cert_file: '/ws_sslfile.pfx',
+        website_ssl_cert_pwd: '1234'
+
+      }
+    end
+
+    on_supported_os.each do |os, os_facts|
+      context "on #{os}" do
+        let(:facts) { os_facts }
+
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_class('dockerapp').with(
+          manage_docker: false
+          ) }
+
+        it { is_expected.to contain_file('/srv/application-data/nettest') }
+        it { is_expected.to contain_file('/srv/application-data/nettest/backups') }
+        it { is_expected.to contain_file('/srv/application-data/nettest/api') }
+        it { is_expected.to contain_file('/srv/application-data/nettest/website') }
+        it { is_expected.to contain_file('/srv/application-config/nettest') }
+        it { is_expected.to contain_file('/srv/application-config/nettest/api') }
+        it { is_expected.to contain_file('/srv/application-config/nettest/configurations') }
+        it { is_expected.to contain_file('/srv/application-config/nettest/ssl') }
+        it { is_expected.to contain_file('/srv/application-config/nettest/website') }
+        it { is_expected.to contain_file('/srv/scripts/nettest') }
+        it { is_expected.to contain_file('/srv/application-log/nettest') }
+        it { is_expected.to contain_file('/srv/application-log/nettest/website') }
+        it { is_expected.to contain_file('/srv/application-log/nettest/api') }
+        it { is_expected.to contain_file('/srv/application-config/nettest/api/certs') }
+        it { is_expected.to contain_file('/srv/application-config/nettest/api/certs/api.pfx') }
+        it { is_expected.to contain_file('/srv/application-config/nettest/website/certs/website.pfx') }
+        it { is_expected.to contain_file('/srv/application-config/nettest/website/certs') }
+
+      end
     end
   end
 end
