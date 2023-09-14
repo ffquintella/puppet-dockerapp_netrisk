bc13dbec51e9f5d9cef1c206e6ae49ca4e708146
Felipe F Quintella <felipe@quintella.com>
2023-09-13 21:41:41 -0300

    INC

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,7 +19,66 @@ class dockerapp_netrisk(
 ) {
 
 include dockerapp::basedirs
+include dockerapp::params
+
+class {"dockerapp":
+  manage_docker => false
+}
+
+    $base_app_config = $::dockerapp::params::config_dir
+    $base_app_home = $::dockerapp::params::data_dir
+    $base_app_scripts = $::dockerapp::params::scripts_dir
+    $base_app_logs = $::dockerapp::params::log_dir
 
 
+    $conf_homedir = "${base_app_home}/${service_name}"
+    $conf_homedir_backups = "${base_app_home}/${service_name}/backups"
+    $conf_homedir_website = "${base_app_home}/${service_name}/website"
+    $conf_homedir_api = "${base_app_home}/${service_name}/api"
+    $conf_configdir = "${base_app_config}/${service_name}"
+    $conf_configdir_website = "${base_app_config}/${service_name}/website"
+    $conf_configdir_api = "${base_app_config}/${service_name}/api"
+    $conf_configdir_configurations = "${base_app_config}/${service_name}/configurations"
+    $conf_configdir_ssl = "${base_app_config}/${service_name}/ssl"
+    $conf_scriptsdir = "${base_app_scripts}/${service_name}"
+    $conf_logsdir = "${base_app_logs}/${service_name}"
+    $conf_logsdir_website = "${base_app_logs}/${service_name}/website"
+    $conf_logsdir_api = "${base_app_logs}/${service_name}/api"
+
+
+    $image_name_api = "ffquintella/netrisk-api:${version}"
+    $image_name_website = "ffquintella/netrisk-website:${version}"
+
+
+    if ! defined(File[$conf_homedir]) {
+      file{ $conf_homedir:
+        ensure  => directory,
+        require => File[$base_app_home],
+      }
+    }
+    if ! defined(File[$conf_homedir_backups]) {
+      file{ $conf_homedir_backups:
+        ensure  => directory,
+        owner   => 33,
+        group   => 33,
+        require => File[$conf_homedir],
+      }
+    } 
+    if ! defined(File[$conf_homedir_website]) {
+      file{ $conf_homedir_website:
+        ensure  => directory,
+        owner   => 33,
+        group   => 33,
+        require => File[$conf_homedir],
+      }
+    } 
+    if ! defined(File[$conf_homedir_api]) {
+      file{ $conf_homedir_api:
+        ensure  => directory,
+        owner   => 33,
+        group   => 33,
+        require => File[$conf_homedir],
+      }
+    }
 
 }
--- spec/classes/dockerapp_netrisk_spec.rb
+++ spec/classes/dockerapp_netrisk_spec.rb
@@ -3,11 +3,30 @@
 require 'spec_helper'
 
 describe 'dockerapp_netrisk' do
+
+  let(:node) { 'node1.test.com' }
+  let(:params) do
+    {
+      service_name: 'nettest',
+      version: '1.4.1',
+
+    }
+  end
+
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
 
       it { is_expected.to compile.with_all_deps }
+      it { is_expected.to contain_class('dockerapp').with(
+        manage_docker: false
+        ) }
+
+      it { is_expected.to contain_file('/srv/application-data/nettest') }
+      it { is_expected.to contain_file('/srv/application-data/nettest/backups') }
+      it { is_expected.to contain_file('/srv/application-data/nettest/api') }
+      it { is_expected.to contain_file('/srv/application-data/nettest/website') }
+
     end
   end
 end
